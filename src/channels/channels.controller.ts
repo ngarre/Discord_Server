ad7e91db87ee3ad05758abd7a60277f929dd93e3
ngarre <a28710@svalero.com>
2026-03-24 11:53:14 +0100

    CRUD completo para channels

---
+++ src/channels/channels.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ChannelsService } from './channels.service';
+import { CreateChannelDto } from './dto/create-channel.dto';
+import { UpdateChannelDto } from './dto/update-channel.dto';
+
+@Controller('channels')
+export class ChannelsController {
+  constructor(private readonly channelsService: ChannelsService) {}
+
+  @Post()
+  create(@Body() dto: CreateChannelDto) {
+    return this.channelsService.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.channelsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.channelsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateChannelDto) {
+    return this.channelsService.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.channelsService.remove(id);
+  }
+}
