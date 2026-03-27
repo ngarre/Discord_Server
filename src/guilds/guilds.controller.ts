0eb0410e71a581c91a8597eb11ec379264f42d23
ngarre <a28710@svalero.com>
2026-03-27 12:09:01 +0100

    añado gestión de miembros de guild con restricción de rol owner

--- src/guilds/guilds.controller.ts
+++ src/guilds/guilds.controller.ts
@@ -17,6 +17,10 @@ import { UpdateGuildDto } from './dto/update-guild.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { CurrentUser } from '../common/decorators/current-user.decorator';
+import { RolesGuard } from '../common/guards/roles.guard';
+import { Roles } from '../common/decorators/roles.decorator';
+import { AddGuildMemberDto } from './dto/add-guild-member.dto';
+import { MemberRole } from '@prisma/client';
 
 @UseGuards(JwtAuthGuard)
 @ApiTags('Guilds')
@@ -45,6 +49,16 @@ export class GuildsController {
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() dto: UpdateGuildDto) {
     return this.guildsService.update(id, dto);
+  }
+
+  @Post(':id/members')
+  @UseGuards(RolesGuard)
+  @Roles(MemberRole.OWNER)
+  addMember(
+    @Param('id', new ParseUUIDPipe()) guildId: string,
+    @Body() dto: AddGuildMemberDto,
+  ) {
+    return this.guildsService.addMember(guildId, dto.userId, dto.role);
   }
 
   @Delete(':id')
